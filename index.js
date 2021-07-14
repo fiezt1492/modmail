fd58b073f1c512bf9a94e97aa01d593d6b629296
Fiezt <40653673+fiezt1492@users.noreply.github.com>
2021-07-14 16:17:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -281,7 +281,7 @@ client.on("message", async message => {
         } 
             else if (command == "rbw") {
                 if (!message.content.startsWith(prefix)) return;
-                message.channel.send('<a:acongablob:864797216992788490><a:acongablob:864797216992788490><a:acongablob:864797216992788490><a:acongablob:864797216992788490>');
+                message.channel.send("<a:acongablob:864797216992788490><a:acongablob:864797216992788490><a:acongablob:864797216992788490><a:acongablob:864797216992788490>");
 
 
     }
